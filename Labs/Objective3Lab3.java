7bd8df6aeb0d21b9feefe3e9b113dbd609239c35
Christina <christina.pitch@gmail.com>
2020-08-03 16:24:39 -0600

    complete objective3lab3

---
+++ Labs/Objective3Lab3.java
@@ -0,0 +1,10 @@
+public class Objective3Lab3 {
+  public static void main(String[] args) {
+    double num1, num2, num3;
+    num1 = 1 + (int)(Math.random() * 6);
+    num2 = 1 + (int)(Math.random() * 6);
+    num3 = 1 + (int)(Math.random() * 6);
+    double sum = num1 + num2 + num3;
+    System.out.println(sum);
+  }
+}
